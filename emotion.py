3b5b0d7d809e1d6e06c47721019c1c99a7f18187
Abishek R <92371861+Abishek673@users.noreply.github.com>
2024-10-23 20:50:59 +0530

    Update emotion.py

--- emotion.py
+++ emotion.py
@@ -1,1 +1,27 @@
+# -*- coding: ulf-8 -*-
+"""
 
+@author: Abishek
+"""
+
+import cv2
+import numpy as np
+from keras.preprocessing import image
+from keras.models import model_from_json
+import face_recognition
+
+
+webcam_video_stream = cv2.VideoCapture(0)
+
+face_exp_model = model_from_json(open("dataset/facial_expression_model_structure.json","r", encoding="utf-8").read())
+face_exp_model.load_weight('ataset/facial_expression_model_weight.h5')
+emotions_label = ('angry', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+all_face_location = []
+
+while True:
+  ret, current_frame = webcam_video_stream.read()
+  current_frame_small = cv2.resize(current_frame, (0,0), fx=0.25,fy=0.25)
+  all_face_location = face_recognition.face_locations(current_frame_small,model='hog')
+
+
+for index
